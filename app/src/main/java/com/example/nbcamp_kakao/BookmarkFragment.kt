71e6f80b368b58e2c3e0a7fd9f125f71c66f78c7
rising111 <ejttotto1234@gmail.com>
2023-09-20 11:38:51 +0900

    Viewpager 프래그먼트 연동 프래그먼트 출력

--- app/src/main/java/com/example/nbcamp_kakao/BookmarkFragment.kt
+++ app/src/main/java/com/example/nbcamp_kakao/BookmarkFragment.kt
@@ -1,10 +1,32 @@
 package com.example.nbcamp_kakao
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nbcamp_kakao.databinding.BookmarkFragmentBinding
 
 class BookmarkFragment: Fragment() {
-
+    private var _binding: BookmarkFragmentBinding? = null
+    private val binding get() = _binding!!
     companion object{
         fun newInstance() = BookmarkFragment()
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BookmarkFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
 }
--- app/src/main/java/com/example/nbcamp_kakao/SearchFragment.kt
+++ app/src/main/java/com/example/nbcamp_kakao/SearchFragment.kt
@@ -1,10 +1,33 @@
 package com.example.nbcamp_kakao
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nbcamp_kakao.databinding.BookmarkFragmentBinding
+import com.example.nbcamp_kakao.databinding.SearchFragmentBinding
 
 class SearchFragment:Fragment() {
+
+    private var _binding: SearchFragmentBinding? = null
+    private val binding get() = _binding!!
     companion object{
         fun newInstance() = SearchFragment()
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = SearchFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 
 }
